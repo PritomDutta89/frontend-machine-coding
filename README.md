2d85f97c0527b2f9472e02693bb398b45b16a5e3
pritom.dutta <pritom.dutta@rahitechnologies.com>
2025-07-21 00:05:17 +0530

    Added Auto‑Complete Search

---
+++ README.md
@@ -0,0 +1,35 @@
+🚀 Frontend Machine Coding Round - Top Challenges with Solutions
+This repository contains some of the most frequently asked frontend machine coding round questions along with their solutions. The primary focus is on implementing the core logic, keeping the UI minimal and functional.
+
+🛠 Tech Stack
+React
+
+Tailwind CSS
+
+🎯 Objective
+The goal is to provide a solid foundation for solving real-world frontend problems commonly asked during interviews. The emphasis is on logic implementation over pixel-perfect UI.
+
+👨‍💻 Contribute
+Feel free to treat this as an open-source project:
+
+Improve the UI
+
+Optimize the logic
+
+Add test cases
+
+Submit enhancements
+
+I'm actively reviewing PRs and will review and merge contributions that add value.
+
+📁 Structure
+Each folder represents a specific machine coding round problem with:
+
+Clean code
+
+Component-based structure
+
+Tailwind for styling
+
+📬 Feedback & Collaboration
+If you have suggestions or improvements, feel free to create an issue or open a pull request.
---
+++ src/auto-complete-search/SearchBar.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+
+const data = ["Apple", "Banana", "Cherry", "Date", "Grapes", "Mango", "Orange"];
+
+const SearchBar = () => {
+  const [text, setText] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  useEffect(() => {
+    console.log("starting1");
+    const timer = setTimeout(() => {
+      console.log("starting2");
+      if (text) {
+        const updatedData = data.filter((item) => {
+          return item.toLowerCase().includes(text.toLowerCase());
+        });
+        setFilteredData(updatedData);
+      } else {
+        setFilteredData([]);
+      }
+    }, 300);
+
+    return () => {
+      console.log("starting3");
+      clearTimeout(timer);
+    };
+  }, [text]);
+
+  const handleKeyDown = (e) => {
+    console.log(e);
+    if (e.key === "ArrowDown") {
+      setActiveIndex((prv) => Math.min(prv + 1, filteredData.length - 1));
+    } else if (e.key === "ArrowUp") {
+      setActiveIndex((prv) => Math.max(prv - 1, 0));
+    } else if (
+      e.key === "Enter" &&
+      activeIndex >= 0 &&
+      activeIndex < filteredData.length - 1
+    ) {
+      setText(filteredData[activeIndex]);
+      setFilteredData([]);
+      setActiveIndex(-1);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center mt-[2rem] w-full">
+      <input
+        type="text"
+        className="border outline-0 p-1 "
+        value={text}
+        onKeyDown={handleKeyDown}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Search..."
+      />
+
+      {filteredData?.length > 0 && (
+        <div className="bg-gray-100 w-[12rem] flex  mt-[1rem] flex-col items-center p-3 h-[7rem] overflow-auto">
+          {filteredData.map((item, index) => (
+            <div
+              className={`p-1 cursor-pointer hover:bg-amber-200 w-full ${
+                index === activeIndex ? "bg-amber-100" : ""
+              }`}
+              key={index}
+              onClick={() => {
+                setText(item);
+                setFilteredData([]);
+                setActiveIndex(-1);
+              }}
+            >
+              <p>{item}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
